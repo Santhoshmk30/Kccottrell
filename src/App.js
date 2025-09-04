870b9b36c4ff7044f3334299c2fb41b55839c29d
M.K.SANTHOSH <121882993+Santhoshmk30@users.noreply.github.com>
2025-09-04 18:11:56 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
 import RequestForm from './Components/Requestform';
 import Certification from './Components/Certification';
 import Approval from './Components/Approval';
 import Register from './Components/Register';
 import Login from './Components/Login';
 import Admin from './Components/Admin';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/request" element={<RequestForm />} />
         <Route path="/certification" element={<Certification />} />
         <Route path="/approval" element={<Approval />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
