a958f61d32e985cf864aacef61bd1d29e69d171f
M.K.SANTHOSH <121882993+Santhoshmk30@users.noreply.github.com>
2025-09-08 14:35:28 +0530

    Update Requestform.js

--- src/Components/Requestform.js
+++ src/Components/Requestform.js
@@ -539,34 +539,6 @@ useEffect(() => {
             />
           </div>
 
-              {/* Transport Mode selection */}
-<div style={styles.transportSection}>
-  <label style={styles.label}>Transport</label>
-  <div style={styles.radioGroup}>
-    {["Air", "Train", "Bus/Taxi/Car"].map((mode) => (
-      <label key={mode} style={styles.radioLabel}>
-        <input
-          type="radio"
-          name="transportMode"
-          value={mode}
-          checked={formData.transportMode === mode}
-          onChange={handleChange}
-          style={styles.radioInput}
-        />
-        {mode}
-      </label>
-    ))}
-  </div>
-
-  {/* Show result */}
-  {formData.transportMode && (
-    <div style={styles.resultBox}>
-      Allowed Class:{" "}
-      <span style={styles.resultText}>{getTravelClass()}</span>
-    </div>
-  )}
-</div>
-
 {/* Ticket Booking By */}
 <div style={styles.field}>
   <label style={styles.label}>Ticket Booked By</label>
@@ -857,6 +829,7 @@ useEffect(() => {
 
 
 export default TripRequestForm;
+
 
 
 
