68b214ece1f907e387dcf6d59d1e632280abb400
M.K.SANTHOSH <121882993+Santhoshmk30@users.noreply.github.com>
2025-09-12 10:37:15 +0530

    Update Requestform.js

--- src/Components/Requestform.js
+++ src/Components/Requestform.js
@@ -1264,6 +1264,21 @@ useEffect(() => {
             style={styles.input1}
           />
 
+ <div style={styles.field}>
+        <label style={styles.label}>Upload Document</label>
+        <input
+          type="file"
+          name="approvalDocument"
+          onChange={(e) =>
+            setFormData((prev) => ({
+              ...prev,
+              approvalDocument: e.target.files[0],
+            }))
+          }
+          style={styles.input1}
+        />
+      </div>
+
         )}
       </div>
 
@@ -1623,6 +1638,7 @@ useEffect(() => {
 
 
 export default TripRequestForm;
+
 
 
 
